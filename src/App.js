5ef9f4f2216bdb7ff25b64960e2c95de291df4e0
Diogo Farkas <diogonan@protonmail.ch>
2020-08-22 11:07:41 -0300

    completed challenges

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from "./services/api";
 
 import "./styles.css";
 
-function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
 
-  async function handleRemoveRepository(id) {
-    // TODO
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories')
+    .then(response => setRepositories(response.data));
+  }, [])
+
+  async function handleAddRepository() {
+    api.post('/repositories', {
+      title: `Desafio ${Date.now()}`,
+      url: "https://github.com/diogoNaN/GoStack-desafio3",
+      techs: ["Node.js", "React JS"]
+    })
+    .then(response => setRepositories([...repositories, response.data]))
+  };
+
+  function handleRemoveRepository(id) {
+    
+    const actualizedRepositories = repositories.filter(repository => {
+      if(repository.id !== id) {
+        return repository
+      }
+    });
+
+    api.delete(`/repositories/${id}`)
+    .then(
+      setRepositories(actualizedRepositories)
+    );
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => (
+            <li key={repository.id}>
+              {repository.title}
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          )
+        )}
+        
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
